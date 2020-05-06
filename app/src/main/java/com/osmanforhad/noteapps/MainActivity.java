513af9052f3df269abff9dc72fe96322e5d3bac5
osman forhad <osmanforhad@pop-os.localdomain>
2020-05-06 17:38:15 +0600

    making navigation view item selected

--- app/src/main/java/com/osmanforhad/noteapps/MainActivity.java
+++ app/src/main/java/com/osmanforhad/noteapps/MainActivity.java
@@ -1,15 +1,18 @@
 package com.osmanforhad.noteapps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     /* variable Declaration **/
     DrawerLayout drawerLayout;
@@ -28,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);//for setup the toolbar
 
+        /* set the navigation view **/
+        nav_view.setNavigationItemSelectedListener(this);
+
         /* create object for actionbar toggle **/
         toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
 
@@ -43,5 +49,21 @@ public class MainActivity extends AppCompatActivity {
         toggle.syncState();
 
     }//end of the onCreate method
+
+    /* method for handle the click of menu button **/
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        /* check which item is click
+        * for Display the message or open the new screen **/
+        switch (item.getItemId()){
+            default:
+                //for display default message
+                Toast.makeText(this,"Coming soon.",Toast.LENGTH_SHORT).show();
+        }
+
+
+        return false;
+    }//end of the onNavigationItemSelected method
 
 }//end of the class
