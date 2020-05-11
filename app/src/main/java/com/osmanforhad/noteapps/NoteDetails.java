5389c964c3d2e94c455a98cf3b1db9938e8ae919
osman forhad <osmanforhad@pop-os.localdomain>
2020-05-11 11:15:21 +0600

    adding back butto into node details screen

--- app/src/main/java/com/osmanforhad/noteapps/NoteDetails.java
+++ app/src/main/java/com/osmanforhad/noteapps/NoteDetails.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
 public class NoteDetails extends AppCompatActivity {
@@ -19,14 +21,37 @@ public class NoteDetails extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /* for back button **/
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-            }
-        });
-    }
 
-}
+            }//end of the onClick method
+
+        });//end of the setOnClickListener
+
+    }//end of the onCreate method
+
+    /*back button as item click**/
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        /* identify back button is clicked as item **/
+        if (item.getItemId() == android.R.id.home){
+
+            /* for sent the user back **/
+            onBackPressed();
+            
+        }//end of the if condition
+
+        return super.onOptionsItemSelected(item);
+
+    }//end of the onOptionsItemSelected method
+
+}//end of the class
