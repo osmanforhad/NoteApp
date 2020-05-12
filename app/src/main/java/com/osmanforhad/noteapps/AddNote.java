6784f6ff2f4c60e4d4cd4dd676e6c01e908f6edb
osman forhad <osmanforhad@pop-os.localdomain>
2020-05-12 14:40:55 +0600

    design add note screen

--- app/src/main/java/com/osmanforhad/noteapps/AddNote.java
+++ app/src/main/java/com/osmanforhad/noteapps/AddNote.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Toast;
 
 public class AddNote extends AppCompatActivity {
+
+    /* variable declaration **/
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +24,16 @@ public class AddNote extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /* get the instance of firebase fire store **/
+        fStore = FirebaseFirestore.getInstance();
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /* for display message
+                * after click the save icon **/
+                Toast.makeText(AddNote.this,"Save btn is clicked.", Toast.LENGTH_SHORT).show();
             }
         });
     }
