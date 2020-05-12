32b0b03772a1d205181d34948d0650731d9f9913
osman forhad <osmanforhad@pop-os.localdomain>
2020-05-12 23:10:10 +0600

    insert note data into firebase database and set validation is emty or not

--- app/src/main/java/com/osmanforhad/noteapps/AddNote.java
+++ app/src/main/java/com/osmanforhad/noteapps/AddNote.java
@@ -2,20 +2,29 @@ package com.osmanforhad.noteapps;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddNote extends AppCompatActivity {
 
     /* variable declaration **/
     FirebaseFirestore fStore;
+    EditText noteTitle, noteContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +36,54 @@ public class AddNote extends AppCompatActivity {
         /* get the instance of firebase fire store **/
         fStore = FirebaseFirestore.getInstance();
 
+        /* initial xml UI **/
+        noteContent = findViewById(R.id.addNoteContent);
+        noteTitle = findViewById(R.id.addNoteTitle);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /* for display message
-                * after click the save icon **/
-                Toast.makeText(AddNote.this,"Save btn is clicked.", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+               /* extract the string **/
+                String nTitle = noteTitle.getText().toString();
+                String nContent = noteContent.getText().toString();
 
-}
+                /* check the user input is empty or not **/
+                if(nTitle.isEmpty() || nContent.isEmpty()){
+
+                  /* for display message to user **/
+                    Toast.makeText(AddNote.this,"Note Can not Save With Empty Field.",Toast.LENGTH_SHORT).show();
+                    return; //its men return user to the same screen
+                }
+
+                //save note
+                DocumentReference docRef = fStore.collection("notes").document();
+                Map<String,Object> note = new HashMap<>();
+                note.put("title",nTitle);
+                note.put("content",nContent);
+
+                docRef.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        /* display message **/
+                        Toast.makeText(AddNote.this,"Note Added.",Toast.LENGTH_SHORT).show();
+
+                        /* redirect user to the main screen **/
+                        onBackPressed();
+                    }//end of the onSuccess method
+
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        /* display message **/
+                        Toast.makeText(AddNote.this,"Error Try again.",Toast.LENGTH_SHORT).show();
+                    }
+                });//end of the addOnFailureListener
+
+            }//end of the onClick method
+
+        });//end of the setOnClickListener
+
+    }//end of the onCreate method
+
+}//end of the class
