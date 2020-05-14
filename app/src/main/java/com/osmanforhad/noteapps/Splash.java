24c75f89a737a4ca6d18d6fe74b8ddb474252324
osman forhad <osmanforhad@pop-os.localdomain>
2020-05-14 22:30:50 +0600

    setup splash time and after that send the user into main content screen

--- app/src/main/java/com/osmanforhad/noteapps/Splash.java
+++ app/src/main/java/com/osmanforhad/noteapps/Splash.java
@@ -2,14 +2,32 @@ package com.osmanforhad.noteapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class Splash extends AppCompatActivity {
+
+    /* variable declaration **/
+    private static int SCREEN_SPLASH_TIME = 2000; /* means 2 Second **/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        /* for sending splash screen to main screen after the 2 sec **/
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* for go to the main Content screen**/
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+            }//end of the run method
+
+        },SCREEN_SPLASH_TIME/* calling the variable **/);//end of the Runnable method
 
     }//end of the onCreate method
 
