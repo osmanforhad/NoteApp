8c8f537e1c40263ada7d34992ec659c431a96949
osman forhad <osmanforhad@pop-os.localdomain>
2020-05-14 21:59:26 +0600

    create and design Login and Register Screen

---
+++ app/src/main/java/com/osmanforhad/noteapps/auth/Login.java
@@ -0,0 +1,16 @@
+package com.osmanforhad.noteapps.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.osmanforhad.noteapps.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
